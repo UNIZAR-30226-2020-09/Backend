a11d5281530e6f250aeb9186d85a8954e71058bc
Raul Herguido <759013@unizar.es>
2020-03-02 09:38:53 +0100

    Fixed beans error and added basic mapping and rest

--- src/main/java/com/Backend/Application/BackendApplication.java
+++ src/main/java/com/Backend/Application/BackendApplication.java
@@ -5,16 +5,26 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class BackendApplication {
 
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
 		//Lo suyo es trabajar con un interfaz para no tener que cambiar
 		//codigo en caso de herencia.
-		User usuario = context.getBean("usuario", User.class);
+		User usuario = context.getBean("usuarioPepe", User.class);
 		System.out.println("Probando beans");
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
 	}
 
 }
