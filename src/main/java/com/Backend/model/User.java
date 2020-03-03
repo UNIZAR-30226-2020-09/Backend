35e5b3d7501de5630970e986f8612723ad71dea4
Aleparicio <761783@unizar.es>
2020-03-03 11:58:53 +0100

    Primer intento de persistencia en PostgreSQL

--- src/main/java/com/Backend/model/User.java
+++ src/main/java/com/Backend/model/User.java
@@ -1,20 +1,55 @@
 package com.Backend.model;
+import javax.persistence.*;
 
+@Entity
 public class User {
 
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Long id;
+    private String mail;
     private String userName;
 
-    public void setUserName(String userName) {
+    protected User() {}
+
+    public User(String userName, String mail) {
         this.userName = userName;
+        this.mail = mail;
     }
 
-    public User(String userName) {
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", mail='" + mail + '\'' +
+                ", userName='" + userName + '\'' +
+                '}';
+    }
+
+    public void setUserName(String userName) {
         this.userName = userName;
     }
 
     public void displayUserInfo(){
         System.out.println("User name is: " + userName);
     }
+
 
 }
 
