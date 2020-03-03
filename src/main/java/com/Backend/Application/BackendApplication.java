b8c408017a961db8728cdd93e7de4efdd14f3df8
alexx99gg <762068@unizar.es>
2020-03-03 17:32:51 +0100

    desperate try

--- src/main/java/com/Backend/Application/BackendApplication.java
+++ src/main/java/com/Backend/Application/BackendApplication.java
@@ -22,57 +22,9 @@ import com.Backend.model.*;
 public class BackendApplication {
 
 	public static void main(String[] args) {
-		//ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
-		//Lo suyo es trabajar con un interfaz para no tener que cambiar
-		//codigo en caso de herencia.
-		//User usuario = context.getBean("usuarioPepe", User.class);
-		//System.out.println("Probando beans");
+
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
 	private static final Logger log = LoggerFactory.getLogger(BackendApplication.class);
-
-	/*@Bean
-	public CommandLineRunner demo(UserRepository repository) {
-		return (args) -> {
-			// save a few customers
-			repository.save(new User("Alejandro", "alex@gg.com"));
-			repository.save(new User("Alejandro", "gogoalex@df.com"));
-			repository.save(new User("Raul H.", "rrrrr@gmail.com"));
-			repository.save(new User("Sergio", "Alejandro"));
-			repository.save(new User("Alejandro", "Magno"));
-
-			// fetch all customers
-			log.info("Customers found with findAll():");
-			log.info("-------------------------------");
-			for (User usuario : repository.findAll()) {
-				log.info(usuario.toString());
-			}
-			log.info("");
-
-			// fetch an individual customer by ID
-			User usuario = repository.findById(1L);
-			log.info("User found with findById(1L):");
-			log.info("--------------------------------");
-			log.info(usuario.toString());
-			log.info("");
-
-			// fetch customers by last name
-			log.info("Customer found with findByLastName('Bauer'):");
-			log.info("--------------------------------------------");
-			repository.findByUserName("Alejandro").forEach(elegido -> {
-				log.info(elegido.toString());
-			});
-			// for (Customer bauer : repository.findByLastName("Bauer")) {
-			//  log.info(bauer.toString());
-			// }
-			log.info("");
-		};
-	}*/
-
-	/*@GetMapping("/hello")
-	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Hello %s!", name);
-	}*/
-
 }
--- src/main/java/com/Backend/model/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.Backend.model;
-import java.util.List;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Long> {
-    List<User> findByUserName(String userName);
-    User findById(long id);
-}
