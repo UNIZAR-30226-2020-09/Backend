c06d9c44968f85e1af8a3699595676b49b7dbbf9
alexx99gg <762068@unizar.es>
2020-03-03 18:53:20 +0100

    hello world page

--- src/main/java/com/Backend/BackendApplication.java
+++ src/main/java/com/Backend/BackendApplication.java
@@ -5,10 +5,17 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
 //@RestController
 public class BackendApplication {
+
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "Hello World!";
+	}
 
 	public static void main(String[] args) {
 		com.Backend.model.User nuevo = new User("alex", "alexcorreo");
