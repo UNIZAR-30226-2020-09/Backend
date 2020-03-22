14d6486d68b2338af7a1615ff213c80cd44fb335
Raul Herguido <759013@unizar.es>
2020-03-22 22:41:54 +0100

    CrossOrigin

--- src/main/java/com/Backend/security/WebSecurityPandora.java
+++ src/main/java/com/Backend/security/WebSecurityPandora.java
@@ -1,11 +1,18 @@
 package com.Backend.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import com.google.common.collect.ImmutableList;
+
+import java.util.Collections;
 
 import static com.Backend.security.Constants.*;
 
@@ -16,7 +23,7 @@ class WebSecurityPandora extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         //http.csrf().disable()
-        http.cors()
+        http.cors();
     }
     
     @Bean
