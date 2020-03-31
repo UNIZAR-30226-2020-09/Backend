ae012390159891a0aad94db42d9217c2331dce7a
alexx99gg <762068@unizar.es>
2020-03-31 16:47:33 +0200

    Merge branch 'master' of /home/a762068/Desktop/Backend with conflicts.

--- src/main/java/com/Backend/controller/PasswordController.java
+++ src/main/java/com/Backend/controller/PasswordController.java
@@ -3,7 +3,6 @@ package com.Backend.controller;
 import com.Backend.exception.CategoryNotFoundException;
 import com.Backend.exception.PasswordNotFoundException;
 import com.Backend.exception.UserNotFoundException;
-import com.Backend.model.Category;
 import com.Backend.model.OwnsPassword;
 import com.Backend.model.Password;
 import com.Backend.model.User;
@@ -14,14 +13,10 @@ import com.Backend.repository.ICatRepo;
 import com.Backend.repository.IOwnsPassRepo;
 import com.Backend.repository.IPassRepo;
 import com.Backend.repository.IUserRepo;
-import lombok.Getter;
-import lombok.Setter;
-import net.minidev.json.JSONArray;
 import net.minidev.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.client.support.InterceptingHttpAccessor;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
@@ -58,62 +53,66 @@ public class PasswordController {
 
         Long userID = getUserIdFromRequest(request);
         JSONObject res = new JSONObject();
-        try{
+        if (passReq.isValid()) {
+            try{
+                Password password = passReq.getAsPassword();
+                password.setCategory(repoCat.findById(passReq.getPasswordCategoryId()).orElseThrow(() -> new CategoryNotFoundException(passReq.getPasswordCategoryId())));
 
-            Password password = passReq.getAsPassword();
-            password.setCategory(repoCat.findById(passReq.getPasswordCategoryId()).orElseThrow(() -> new CategoryNotFoundException(passReq.getPasswordCategoryId())));
-            User user = repoUser.findById(userID).orElseThrow(() -> new UserNotFoundException(userID));
+                User user = repoUser.findById(userID).orElseThrow(() -> new UserNotFoundException(userID));
 
-            //Se comprueba que el usuario no tiene una contraseña con el mismo nombre
-            List<OwnsPassword> allops = repoOwnsPass.findAllByUser(user);
-            for (OwnsPassword i:allops){
-                System.out.println((i.getPassword()).getPasswordName());
-                if((i.getPassword()).getPasswordName().equals(password.getPasswordName())){
-                    res.put("statusText", "Ya existe una contraseña con el mismo nombre para el usuario");
-                    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(res);
+                //Se comprueba que el usuario no tiene una contraseña con el mismo nombre
+                List<OwnsPassword> allops = repoOwnsPass.findAllByUser(user);
+                for (OwnsPassword i:allops){
+                    System.out.println((i.getPassword()).getPasswordName());
+                    if((i.getPassword()).getPasswordName().equals(password.getPasswordName()) &&
+                            (i.getPassword()).getUserName().equals(password.getUserName())){
+                        res.put("statusText", "Ya existe una contraseña con el mismo nombre y usuario");
+                        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(res);
+                    }
                 }
+                repoPass.save(password);
+                OwnsPassword ownsp = new OwnsPassword(user, password, 1);
+                repoOwnsPass.save(ownsp);
+                res.put("statusText", "Contraseña insertada");
+                return ResponseEntity.status(HttpStatus.OK).body(res);
+            }
+            catch (CategoryNotFoundException e) {
+                e.printStackTrace();
+                res.put("statusText", "Categoria no encontrada");
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(res);
             }
 
-            repoPass.save(password);
-            OwnsPassword ownsp = new OwnsPassword(user, password, 1);
-            repoOwnsPass.save(ownsp);
-            return ResponseEntity.status(HttpStatus.OK).body(res);
-        }
-        catch (CategoryNotFoundException e) {
-            e.printStackTrace();
-            res.put("statusText", "Categoría no encontrada.");
+
+        } else{
+            res.put("statusText", "Faltan campos.");
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(res);
         }
     }
 
     @GetMapping(LISTAR_PASSWORDS_USUARIO_URL)
     public ResponseEntity<JSONObject> listar(HttpServletRequest request)
-            throws UserNotFoundException {
+            throws UserNotFoundException, PasswordNotFoundException {
 
         Long idUser = getUserIdFromRequest(request);
         User user = repoUser.findById(idUser).orElseThrow(() -> new UserNotFoundException(idUser));
+        JSONObject res = new JSONObject();
+
         List<OwnsPassword> allops = repoOwnsPass.findAllByUser(user);
-
-        JSONArray allpass = new JSONArray();
-
+        List<String> allpass = new ArrayList<>();
         for (OwnsPassword i:allops){
             PasswordResponse pres = new PasswordResponse(i);
-            JSONObject a = new JSONObject();
-            a.put("passId", pres.getPassId());
-            a.put("passwordName", pres.getPasswordName());
-            a.put("catId", pres.getCatId());
-            a.put("categoryName", pres.getCategoryName());
-            a.put("rol", pres.getRol());
-            a.put("optionalText",pres.getOptionalText());
-            a.put("userName", pres.getUserName());
-            allpass.add(a);
+            allpass.add(pres.toString());
         }
-
-        JSONObject res = new JSONObject();
         res.put("passwords", allpass);
+        res.put("statusText", "Contraseñas encontradas");
         return ResponseEntity.status(HttpStatus.OK).body(res);
+
+
     }
 
+    /*
+     * Método para eliminar usuario
+     */
     @DeleteMapping(ELIMINAR_PASSWORD_URL)
     public ResponseEntity<JSONObject> eliminar(HttpServletRequest request,
                                                @RequestBody DeleteByIdRequest deleteIdReq) throws UserNotFoundException, PasswordNotFoundException {
@@ -137,6 +136,7 @@ public class PasswordController {
                 //No eres el usuario creador
                 repoOwnsPass.delete(ops);
             }
+            res.put("statusText", "Contraseña eliminada");
             return ResponseEntity.status(HttpStatus.OK).body(res);
         } else{
             res.put("statusText", "No autorizado");
